ca0e31d7bf9e5c044a8b123b963b194fab6bd197
Luis Cabanzon Gutierrez <luiscabanzon@gmail.com>
2016-12-10 23:07:14 +0100

    Updated: binary_backpack

--- evolutionary_computation/binary_backpack/A1_code.py
+++ evolutionary_computation/binary_backpack/A1_code.py
@@ -8,18 +8,48 @@ def load_items(file):
 items = load_items('item_list.txt')
 print(items)
 
-def random_solution(n_genes):
-	from random import choice
+def random_solution(n_genes, seed):
+	from random import seed, choice
+	seed(seed)
 	solution = []
 	for i in range(n_genes):
 		solution.append(choice([0,1]))
 	return solution
 
-def create_population(size,element_generator,n_genes):
+def fitness(solution, ref_list, backpack_capacity):
+	fitness_score = 0
+	total_weight = 0
+	for i in range(len(solution)):
+		gene = solution[i]
+		weigth = float(ref_list[i][2])
+		total_weight += weigth * gene
+		#print(total_weight)
+
+		if total_weight > backpack_capacity:
+			return fitness_score
+
+		value = float(ref_list[i][1])
+		fitness_score += gene * (value/weigth)
+	return fitness_score
+
+def create_population(size,element_generator,n_genes, seed):
 	population = []
 	for i in range(size):
-		population.append(element_generator(n_genes))
+		population.append(element_generator(n_genes, seed))
 	return population
 
-population = create_population(100,random_solution, len(items))
+def termination_condition():
+	pass
+
+
+#################
+# TEST
+#################
+population = create_population(100,random_solution, len(items), 42)
 print(population)
+
+print(fitness([0,1,1,0,0,1,0], items, 100)) # = 7
+
+print(fitness([1,1,1,0,0,0,0], items, 100)) # = 4.5
+
+print(fitness([1,1,1,1,1,1,1], items, 100)) # = 4.5
