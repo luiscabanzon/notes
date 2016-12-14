c3e135f14546b86f072967d918b312a8b8eca6b2
Luis Cabanzon Gutierrez <luiscabanzon@gmail.com>
2016-12-14 22:41:08 +0100

    Updated: binary_backpack

--- evolutionary_computation/binary_backpack/A1_code_01.py
+++ evolutionary_computation/binary_backpack/A1_code_01.py
@@ -4,20 +4,6 @@ __author__ = 'Luis Cabanzón'
 from random import choice, sample, uniform
 import json
 
-def load_items(file, sort_items=True):
-    item_list = []
-    f = open(file).readlines()
-    for line in f:
-        item = dict()
-        item['value'] = float(line[0])
-        item['weight'] = float(line[1])
-        item_list.append(item)
-    if sort_items:
-        item_list = sorted(item_list, key=lambda x: x['value'] / x['weight'], reverse=True)
-    return item_list
-
-
-params = json.loads(open('parameters.json', 'r').read())
 
 def create_item_list(size, sort_items=True):
     item_list = dict()
@@ -36,9 +22,6 @@ def create_item_list(size, sort_items=True):
     print("Backpack capacity: %s" % item_list['backpack_capacity'])
     return item_list
 
-# items = load_items('item_list_2.txt', params['sort_items'])
-items = create_item_list(params['item_list_size'], params['sort_items'])
-
 
 def random_solution(n_genes):
     solution = []
@@ -48,22 +31,23 @@ def random_solution(n_genes):
 
 
 def create_population(size, solution_generator, n_genes):
+    # Create a population of N solutions with n binary elements each
     population = []
     for i in range(size):
         population.append(solution_generator(n_genes))
     return population
 
 
-def fitness(solution, ref_list, backpack_capacity):
+def fitness(solution, ref_list):
+    # Returns a fitness score
     fitness_score = 0
     total_weight = 0
     for i in range(len(solution)):
         gene = solution[i]
         weight = float(ref_list['items'][i]['weight'])
         total_weight += weight * gene
-        # print(total_weight)
 
-        if total_weight > backpack_capacity:
+        if total_weight > ref_list['backpack_capacity']:
             return fitness_score
 
         value = float(ref_list['items'][i]['value'])
@@ -73,15 +57,13 @@ def fitness(solution, ref_list, backpack_capacity):
 
 def tournament_selector(population, n_selections, fitness_function,
                         tournament_size=2, choose_fittest=1):
+    # Selects K solutions from the population, using the tournament technique
     pop_index = list(range(len(population)))
     selected_solutions = []
 
     for i in range(n_selections):
         candidates = [population[e] for e in sample(pop_index, tournament_size)]
-        # DELETE
-        #print candidates
-        #print map(lambda x: sum(x), candidates)
-        sorted_candidates = sorted(candidates, key=lambda x: fitness_function(x, items, 65), reverse=True)
+        sorted_candidates = sorted(candidates, key=lambda x: fitness_function(x, items), reverse=True)
         winner = False
         for j in sorted_candidates:
             if uniform(0, 1) <= choose_fittest:
@@ -90,12 +72,11 @@ def tournament_selector(population, n_selections, fitness_function,
         if not winner:
             winner = sorted_candidates[-1]
         selected_solutions.append(winner)
-        # DELETE
-        # print sum(winner)
     return selected_solutions
 
 
 def onepoint_crossover(parent_A, parent_B, crossover_rate=0.5):
+    # Generates a children from two different parents, using the onepoint crossover technique
     # print parent_A, parent_B
     parent_size = len(parent_A)
     onepoint = choice(range(parent_size))
@@ -110,41 +91,22 @@ def onepoint_crossover(parent_A, parent_B, crossover_rate=0.5):
     return [children_A, children_B]
 
 
-def mutate(solution, mutation_rate=1.0 / len(items)):
-    return [pow(1, x) if uniform(0, 1) <= mutation_rate else x for x in solution]
+def mutate(solution, mutation_rate):
+    # Receives a solution and returns it back with mutations (or not, depending on the chance rate)
+    # Note pow(1,1)
+    return [pow(0, x) if uniform(0, 1) <= mutation_rate else x for x in solution]
 
 
-def evaluate_population(population, fitness_function, items, backpack_capacity):
+def evaluate_population(population, fitness_function, items):
     # print type(population)
     # print len(population)
-    return sum([fitness_function(solution, items, backpack_capacity) for solution in population]) / float(len(population))
+    return sum([fitness_function(solution, items) for solution in population]) / float(len(population))
 
 
-print(items)
+params = json.loads(open('parameters.json', 'r').read())
+items = create_item_list(params['item_list_size'], params['sort_items'])
 population = create_population(100, random_solution, items['list_size'])
-'''
-#################
-# TEST
-#################
 
-print(population)
-
-print(fitness([0, 1, 1, 0, 0, 1, 0], items, 100))  # = 7
-
-print(fitness([1, 1, 1, 0, 0, 0, 0], items, 100))  # = 4.5
-
-print(fitness([1, 1, 1, 1, 1, 1, 1], items, 100))  # = 4.5
-
-new_population = tournament_selector(population, 100, fitness)
-print(new_population)
-
-print(onepoint_crossover([1, 1, 1, 1, 1], [0, 0, 0, 0, 0], 1))
-print(onepoint_crossover([1, 1, 1, 1, 1], [0, 0, 0, 0, 0], 0))
-
-print(map(lambda x: mutate(x), new_population))
-
-print(evaluate_population(new_population, fitness, items, 100))
-'''
 performance = []
 for R in range(params['iterations']):
     new_population = tournament_selector(population, params['population_size'], fitness, tournament_size=params['tournament_size'])
@@ -153,17 +115,17 @@ for R in range(params['iterations']):
     children = []
     for p in range(len(parents_A) - 1):
         children += onepoint_crossover(parents_A[p], parents_B[p], params['crossover_rate'])
-    mutated_children = [mutate(child) for child in children]
-    best_solution = sorted(new_population, key=lambda x: fitness(x, items, params['backpack_capacity']), reverse=True)[0]
+    mutated_children = [mutate(child, 1.0/len(items['items'])) for child in children]
+    best_solution = sorted(new_population, key=lambda x: fitness(x, items), reverse=True)[0]
     population = mutated_children + [best_solution]
     # print len(population)
     # print population
-    score = evaluate_population(population, fitness, items, params['backpack_capacity'])
+    score = evaluate_population(population, fitness, items)
     print("Generation %s. Avg. Score: %s" % (R, score))
     print "Best Solution (Gen. %s): %s" % (R, best_solution)
     performance.append(score)
 
-best_solution = sorted(new_population, key=lambda x: fitness(x, items, params['backpack_capacity']), reverse=True)[0]
+best_solution = sorted(new_population, key=lambda x: fitness(x, items), reverse=True)[0]
 print "Best Solution: %s" % best_solution
 # for p in performance:
 # print p
